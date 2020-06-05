1240fb0dce3e0b8b347d1e545ed3d79bdfcec9ba
CaptKen <60070121@kmitl.ac.th>
2020-06-05 23:35:27 +0700

    POST javaScript

--- src/main/java/com/example/order/OrderController.java
+++ src/main/java/com/example/order/OrderController.java
@@ -13,8 +13,10 @@ import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,50 +31,29 @@ public class OrderController {
 	@Autowired
 	private OrderRepository pR;
 	
-	private static String readTextFile() 
-    {
-        StringBuilder contentBuilder = new StringBuilder();
-        String filePath = "order(s).txt";
-        
-        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8)) 
-        {
-            stream.forEach(s -> contentBuilder.append(s));
-        }
-        catch (IOException e) 
-        {
-            e.printStackTrace();
-        }
-        return contentBuilder.toString();
-    }
+//	private static String readTextFile() 
+//    {
+//        StringBuilder contentBuilder = new StringBuilder();
+//        String filePath = "order.txt";
+//        
+//        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8)) 
+//        {
+//            stream.forEach(s -> contentBuilder.append(s));
+//        }
+//        catch (IOException e) 
+//        {
+//            e.printStackTrace();
+//        }
+//        return contentBuilder.toString();
+//    }
 	
 	List<Order> orders = new ArrayList();
-	@GetMapping("/create")
-	public void createOrdersObject(@RequestParam("textFile")  File txt) {
-		
-		
-//		String plainText = readTextFile();
-//        String testText = "0002|000002|AirMax|5600.00|30|13.00|168000.00|0003|000003|Air Jordan|5600.00|10|13.00|560000.00";
-		
-		String[] strs = null;
-		try {
-		      File myObj = txt;
-		      Scanner myReader = new Scanner(myObj);
-		      while (myReader.hasNextLine()) {
-		        String data = myReader.nextLine();
-		        strs = data.split("\\|");
-//		        System.out.println(data);
-		      }
-		      myReader.close();
-		    } catch (FileNotFoundException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-		
-		
-		
+	@PostMapping("/create")
+	public void createOrdersObject(@RequestParam("inText") String[] txt) {		
+		String[] strs = txt;	
 		int index = 0;
+		
         for(int i=0; i<(strs.length)/7; i++) {
-        	
     		int order_id = Integer.parseInt(strs[index]);
     		int product_code = Integer.parseInt(strs[index+1]);
     		String product_name = strs[index+2];
@@ -91,8 +72,55 @@ public class OrderController {
         	pR.save(o);
         	index += 7;
         }
-        System.out.print(orders);
-	}
+    }
+	
+//	@PostMapping("/create")
+//	public void createOrdersObject(@RequestParam("textFile")  File txt) {
+//		
+//		
+//		String plainText = readTextFile();
+////        String testText = "0002|000002|AirMax|5600.00|30|13.00|168000.00|0003|000003|Air Jordan|5600.00|10|13.00|560000.00";
+//		
+//		String[] strs = null;
+//		try {
+//		      File myObj = txt;
+//		      Scanner myReader = new Scanner(myObj);
+//		      while (myReader.hasNextLine()) {
+//		        String data = myReader.nextLine();
+//		        strs = plainText.split("\\|");
+////		        System.out.println(data);
+//		      }
+//		      myReader.close();
+//		    } catch (FileNotFoundException e) {
+//		      System.out.println("An error occurred.");
+//		      e.printStackTrace();
+//		    }
+//		
+//		
+//		
+//		int index = 0;
+//        for(int i=0; i<(strs.length)/7; i++) {
+//        	
+//    		int order_id = Integer.parseInt(strs[index]);
+//    		int product_code = Integer.parseInt(strs[index+1]);
+//    		String product_name = strs[index+2];
+//    		int price_per_unit = Integer.parseInt(strs[index+3]);
+//    		int unit = Integer.parseInt(strs[index+4]);
+//    		float vat = Float.parseFloat(strs[index+5]);
+//    		int total_price = Integer.parseInt(strs[index+6]);
+//        	Order o = new Order();
+//        	o.setOrderID(order_id);
+//        	o.setProduct_code(product_code);
+//        	o.setProduct_name(product_name);
+//        	o.setPrice_per_unit(price_per_unit);
+//        	o.setUnit(unit);
+//        	o.setVat(vat);
+//        	o.setTotal_price(total_price);
+//        	pR.save(o);
+//        	index += 7;
+//        }
+//        System.out.print(orders);
+//	}
    
 	 @GetMapping("/order")
 	 public List<Order> getAllOrder() {
